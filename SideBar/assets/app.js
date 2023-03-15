ee05c5e781ff46e02f5fc419be60229bb4b0510b
john <johnsantosh2@gmail.com>
2023-03-15 09:58:34 +0530

    side bar fully updated build

--- SideBar/assets/app.js
+++ SideBar/assets/app.js
@@ -1,12 +1,5 @@
-window.addEventListener("DOMContentLoaded", () => {
-  let year = document.querySelector(".year");
-  let date = new Date();
-  year.textContent = date.getFullYear(".menu-icon");
-});
-
 let crossBtn = document.querySelector(".fa-times");
 let menuBtn = document.querySelector(".menu-icon");
-crossBtn.addEventListener("click", () => {});
 let nav = document.querySelector("nav");
 
 menuBtn.addEventListener("click", () => {
@@ -15,4 +8,10 @@ menuBtn.addEventListener("click", () => {
 
 crossBtn.addEventListener("click", () => {
   nav.classList.toggle("un-hide");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  let year = document.querySelector(".year");
+  let date = new Date();
+  year.textContent = date.getFullYear(".menu-icon");
 });
